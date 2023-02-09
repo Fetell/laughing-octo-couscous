3690a214e933c8163656bad887489ec1bf202264
Fetell <fetell.eee@gmail.com>
2023-02-09 14:26:04 +0300

    Level setup

---
+++ sources/main.py
@@ -0,0 +1,32 @@
+import sys
+import pygame
+from settings import *
+from level import Level
+
+
+class Game:
+    def __init__(self):
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('My Game')
+
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
